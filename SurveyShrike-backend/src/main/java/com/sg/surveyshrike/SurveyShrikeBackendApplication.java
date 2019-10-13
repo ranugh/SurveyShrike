68f5148f486ba8be9a343239bbd754b04b3a75d5
ranugh <ranu.ghosh97@gmail.com>
2019-10-13 14:15:07 +0530

    Committed To setup the project and cretaed login page

---
+++ SurveyShrike-backend/src/main/java/com/sg/surveyshrike/SurveyShrikeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sg.surveyshrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyShrikeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyShrikeBackendApplication.class, args);
+	}
+
+}
---
+++ SurveyShrike-backend/src/main/java/com/sg/surveyshrike/model/User.java
@@ -0,0 +1,21 @@
+package com.sg.surveyshrike.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String name;
+	private String password;
+	private String emailId;
+	
+}
